e070416a1e7a8c4f86b03abdae76314ccda68a25
kinifi <chrisfigueroa16@gmail.com>
2016-09-25 17:28:56 -0700

    first full commit

---
+++ Assets/Scripts/DebugSystem/Debugger.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Debugger : MonoBehaviour {
+
+	private bool m_debuggerOn = false;
+
+	public List<Log> m_logs = new List<Log>();
+
+	private string m_viewingStackTrace = "Select A Log/Error/Warning to view the StackTrace";
+	private Vector2 scrollPosition;
+
+	// Use this for initialization
+	void Start () {
+
+		DontDestroyOnLoad(this.gameObject);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//check if the user has pressed tab or not to bring up the debugger
+		if(Input.GetKeyDown(KeyCode.Tab))
+		{
+			ToggleDebugger();
+		}
+	}
+
+	private void ToggleDebugger()
+	{
+		m_debuggerOn = !m_debuggerOn;
+    	Debug.Log("Toggling Debugger");
+	}
+
+    void OnGUI()
+    {
+
+    	if(m_debuggerOn == false)
+    		return;
+
+		GUILayout.Box("", GUILayout.Width(Screen.width), GUILayout.Height(Screen.height));
+
+    	GUILayout.BeginArea(new Rect(0,0, Screen.width, Screen.height));
+
+    	GUILayout.Button(Application.productName + " | Tab = Open/Close Debugger | Copy & Pasting Stacktraces Allowed");
+
+    	if(m_logs.Count != 0)
+    	{
+
+	    	GUILayout.BeginVertical();
+
+	    	scrollPosition = GUILayout.BeginScrollView(scrollPosition);
+
+	    	
+		    	for (int i = 1; i <= m_logs.Count; i++)
+		        {
+
+		        	string colorStartString;
+		        	//check what log type it is so we can assign a color to the log
+		        	if(m_logs[i-1].type.ToString().ToLower() == "warning")
+		        	{
+		        		colorStartString = "yellow";
+		        	}
+		        	else if(m_logs[i-1].type.ToString().ToLower() == "log")
+		        	{
+		        		colorStartString = "white";
+		        	}
+		        	else
+		        	{
+		        		colorStartString = "red";
+		        	}
+		        	
+		        	//put the log into a single string
+		            string fullLog = "<color=" + colorStartString + ">" + m_logs[i-1].type.ToString() + "</color>" + " : " + m_logs[i-1].log;
+		            
+		            //display the log inside a button so we can select it and see the full stack trace
+		            if(GUILayout.Button(fullLog))
+		            {
+		            	m_viewingStackTrace = m_logs[i-1].stackTrace;
+		            }
+
+		        }
+	    	
+
+	        GUILayout.EndScrollView();
+
+	        GUILayout.TextArea(m_viewingStackTrace);
+
+	        GUILayout.EndVertical();
+
+    	}
+    	else
+    	{
+    		GUILayout.Label("No Logs To Display");
+    	}
+
+    	GUILayout.EndArea();
+    }
+
+
+    void OnEnable() 
+    {
+        Application.logMessageReceived += HandleLog;
+    }
+    
+    void OnDisable()
+    {
+        Application.logMessageReceived -= HandleLog;
+    }
+    
+    void HandleLog(string logString, string stackTrace, LogType type) {
+        
+        Log newLog = new Log();
+        newLog.log = logString;
+        newLog.stackTrace = stackTrace;
+        newLog.type = type;
+        m_logs.Add(newLog);
+
+    }
+
+}
---
+++ Assets/Scripts/JSON/TestScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using TinyJSON;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class TestScript : MonoBehaviour {
+
+    //the list of GameObjects we need to serialize
+    public List<Entity> m_Entities = new List<Entity>();
+
+    /// Context Menu allows you to right click a script and execute it from the inspector
+    [ContextMenu("Json Class Test")]
+    public void JSONTest()
+    {
+        //Create 5 GameObjects
+        for (int i = 0; i < 5; i++)
+        {
+            //create a new object
+            Entity newEntity = new Entity();
+            //set the name. 
+            newEntity.m_name = "name" + i;
+            //set the position
+            newEntity.m_position.x = i;
+            newEntity.m_position.y = i;
+            //this object will not be static
+            newEntity.m_static = false;
+            //add the new Entity to the list of Entities 
+            m_Entities.Add(newEntity);
+        }
+
+        //serialize all these objects into a pretty json string
+        string testClassJson = JSON.Dump(m_Entities, true);
+
+        //create a save location thats inside of the Assets folder
+        string saveLocation = Application.dataPath + "/" + "JSONTest.json";
+
+        //save this into a file
+        File.WriteAllText(saveLocation, testClassJson);
+
+#if UNITY_EDITOR
+        //refresh the project view if we are in the editor only
+        AssetDatabase.Refresh();
+#endif
+
+    }
+
+}
+
+public class Entity
+{
+    public Vector2 m_position;
+    public string m_name;
+    public bool m_static;
+}
