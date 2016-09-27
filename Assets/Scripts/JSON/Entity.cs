4d8ea0f27278f3c94c9449ed609fe59d13cc3548
Chris Figueroa <chrisfigueroa16@gmail.com>
2016-09-27 15:51:26 -0700

    added doc info to Entity.cs

--- Assets/Scripts/JSON/Entity.cs
+++ Assets/Scripts/JSON/Entity.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+﻿/*
+ * Entity is how we serialize GameObjects by name, position, and static status. 
+ * Using this to match with a prefab then instantiate the prefab would be the best way since we can't serialize all the data on a GameObject
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
