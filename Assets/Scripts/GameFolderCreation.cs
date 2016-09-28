3cc2387dd783d8679891590783c947ea07796ce9
kinifi <chrisfigueroa16@gmail.com>
2016-09-27 21:39:51 -0700

    levelselection screen, folder creation, splash screen
    - Level selection screen now searches for files in GameLevels
    - Splash screen now creates required folder if they dont exist

---
+++ Assets/Scripts/GameFolderCreation.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class GameFolderCreation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+		CreateDirectory("GameLevels");
+		CreateDirectory("GameSaves");
+		RefreshDatabase();
+	}
+	
+	//creates a directory with the given name in the Assets folder
+	public void CreateDirectory(string name)
+	{
+		if(Directory.Exists(Application.dataPath + "/" + name) == false)
+		{
+			Directory.CreateDirectory(Application.dataPath + "/" + name);
+			Debug.Log("Folder Created: " + name);
+		}
+		else
+		{
+			Debug.Log("Folder Already Exists: " + name);
+		}
+	}
+
+	private void RefreshDatabase()
+	{
+		#if UNITY_EDITOR
+        //refresh the project view if we are in the editor only
+        AssetDatabase.Refresh();
+		#endif
+	}
+
+
+}
---
+++ Assets/Scripts/LevelSelectionMenu.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+
+public class LevelSelectionMenu : MonoBehaviour {
+
+	public string[] m_levels;
+
+	private Vector2 scrollPosition;
+
+	// Use this for initialization
+	void Start ()
+	{
+		GetLevels();
+	}
+	
+	public void OnGUI()
+	{
+		GUILayout.BeginArea(new Rect(Screen.width/3, 0, Screen.width/3, Screen.height));
+		GUILayout.Label("Level Selection - " + m_levels.Length);
+		GUILayout.BeginVertical();
+		scrollPosition = GUILayout.BeginScrollView(scrollPosition);
+		for (int i = 1; i <= m_levels.Length; i++)
+        {
+        	//check to make sure if we are in the editor we ignore .meta files
+        	if(!m_levels[i-1].Contains(".meta"))
+        	{
+	        	if(GUILayout.Button(Path.GetFileNameWithoutExtension(m_levels[i-1]), GUILayout.Height(40), GUILayout.Width(Screen.width/3-40)))
+	        	{
+	        		Debug.Log("File Path: " + m_levels[i-1]);
+	        	}
+        	}
+        }
+        GUILayout.EndScrollView();
+        GUILayout.EndVertical();
+        GUILayout.EndArea();
+
+	}
+
+	public void GetLevels()
+	{
+		m_levels = Directory.GetFiles(Application.dataPath + "/GameLevels/", "*.json");
+
+		Debug.Log("Levels Found: " + m_levels.Length);
+	}
+
+	public List<string> GetLevelNames()
+	{
+
+		List<string> levels = new List<string>();
+
+	 	for (int i = 1; i <= m_levels.Length; i++)
+        {
+            levels.Add(m_levels[i]);
+        }
+
+        return levels;
+	}
+
+}
